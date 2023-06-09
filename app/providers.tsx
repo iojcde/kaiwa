26370423af823ec91e9ef4bae606f6f46b2ca295
Jeeho Ahn <io@jcde.xyz>
2023-06-09 19:35:14 +0900

    fix types

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
+import { ReactNode } from "react";
 
-export const Providers = ({ children }) => {
+export const Providers: React.FC<{ children: ReactNode }> = ({ children }) => {
   return <SessionProvider>{children}</SessionProvider>;
 };
