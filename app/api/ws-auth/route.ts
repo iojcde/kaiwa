d04cbc3810f628d2ced10ac25e88cad93ffb8bac
Jeeho Ahn <io@jcde.xyz>
2023-07-04 19:00:38 +0900

    asdf2

--- app/api/ws-auth/route.ts
+++ app/api/ws-auth/route.ts
@@ -7,7 +7,7 @@ const wsAuth = async (req: Request) => {
   if (auth !== process.env.WSAUTH_SECRET) {
     return new Response("Unauthorized", { status: 401 });
   }
-  const { check, userID } = await req.json<{ check: string; userID: string }>();
+  const { check, userID } = await req.json();
 
   const session = await db.session.findFirst({ where: { userId: userID } });
 
