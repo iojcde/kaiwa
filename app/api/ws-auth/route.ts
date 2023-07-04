f181186c9490f347f465963dd24a81993cf96c61
Jeeho Ahn <io@jcde.xyz>
2023-07-04 19:13:59 +0900

    asdf

--- app/api/ws-auth/route.ts
+++ app/api/ws-auth/route.ts
@@ -1,15 +1,18 @@
 import { db } from "@/lib/db";
 
 const wsAuth = async (req: Request) => {
-  console.log("wow");
   const auth = req.headers.get("auth");
-
+  console.log("auth:", auth);
   if (auth !== process.env.WSAUTH_SECRET) {
     return new Response("Unauthorized", { status: 401 });
   }
   const { check, userID } = await req.json();
 
   const session = await db.session.findFirst({ where: { userId: userID } });
+
+  console.log("session", session.wsToken);
+
+  console.log("check:", check);
 
   if (session.wsToken == check) {
     return new Response("OK", { status: 200 });
